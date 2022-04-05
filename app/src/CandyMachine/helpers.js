ea93e435298cc768327a72fcd4ef63e999f20edc
Che Coelho <checoelho@gmail.com>
2022-04-05 18:01:52 +0200

    minor changes

--- app/src/CandyMachine/helpers.js
+++ app/src/CandyMachine/helpers.js
@@ -1,74 +1,74 @@
-import { web3 } from '@project-serum/anchor';
-import * as anchor from '@project-serum/anchor';
-import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
-import { SystemProgram } from '@solana/web3.js';
+import { web3 } from '@project-serum/anchor'
+import * as anchor from '@project-serum/anchor'
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
+import { SystemProgram } from '@solana/web3.js'
 import {
   LAMPORTS_PER_SOL,
   SYSVAR_RENT_PUBKEY,
   TransactionInstruction,
-} from '@solana/web3.js';
+} from '@solana/web3.js'
 
 // CLI Properties Given to us
 const candyMachineProgram = new web3.PublicKey(
   'cndy3Z4yapfJBmL3ShUp5exZKqR3z33thTzeNMm2gRZ'
-);
+)
 
 const TOKEN_METADATA_PROGRAM_ID = new web3.PublicKey(
   'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s'
-);
+)
 
 const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new web3.PublicKey(
   'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL'
-);
+)
 
 const CIVIC = new anchor.web3.PublicKey(
-  'gatem74V238djXdzWnJf94Wo1DcnuGkfijbf3AuBhfs',
-);
+  'gatem74V238djXdzWnJf94Wo1DcnuGkfijbf3AuBhfs'
+)
 
 const toDate = (value) => {
   if (!value) {
-    return;
+    return
   }
 
-  return new Date(value.toNumber() * 1000);
-};
+  return new Date(value.toNumber() * 1000)
+}
 
 const numberFormater = new Intl.NumberFormat('en-US', {
   style: 'decimal',
   minimumFractionDigits: 2,
   maximumFractionDigits: 2,
-});
+})
 
 const formatNumber = {
   format: (val) => {
     if (!val) {
-      return '--';
+      return '--'
     }
 
-    return numberFormater.format(val);
+    return numberFormater.format(val)
   },
   asNumber: (val) => {
     if (!val) {
-      return undefined;
+      return undefined
     }
 
-    return val.toNumber() / LAMPORTS_PER_SOL;
+    return val.toNumber() / LAMPORTS_PER_SOL
   },
-};
+}
 
-const getAtaForMint = async (mint, buyer)=> {
+const getAtaForMint = async (mint, buyer) => {
   return await anchor.web3.PublicKey.findProgramAddress(
     [buyer.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), mint.toBuffer()],
-    SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID,
-  );
-};
+    SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID
+  )
+}
 
 const getNetworkExpire = async (gatekeeperNetwork) => {
   return await anchor.web3.PublicKey.findProgramAddress(
     [gatekeeperNetwork.toBuffer(), Buffer.from('expire')],
-    CIVIC,
-  );
-};
+    CIVIC
+  )
+}
 
 const getNetworkToken = async (wallet, gatekeeperNetwork) => {
   return await anchor.web3.PublicKey.findProgramAddress(
@@ -78,15 +78,15 @@ const getNetworkToken = async (wallet, gatekeeperNetwork) => {
       Buffer.from([0, 0, 0, 0, 0, 0, 0, 0]),
       gatekeeperNetwork.toBuffer(),
     ],
-    CIVIC,
-  );
-};
+    CIVIC
+  )
+}
 
 function createAssociatedTokenAccountInstruction(
   associatedTokenAddress,
   payer,
   walletAddress,
-  splTokenMintAddress,
+  splTokenMintAddress
 ) {
   const keys = [
     {
@@ -124,12 +124,12 @@ function createAssociatedTokenAccountInstruction(
       isSigner: false,
       isWritable: false,
     },
-  ];
+  ]
   return new TransactionInstruction({
     keys,
     programId: SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID,
     data: Buffer.from([]),
-  });
+  })
 }
 
 export {
@@ -143,4 +143,4 @@ export {
   getNetworkExpire,
   getNetworkToken,
   createAssociatedTokenAccountInstruction,
-};
+}
